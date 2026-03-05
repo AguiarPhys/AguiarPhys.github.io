d8075e6bf60551a2f597d689686185e211d49e7e
AguiarPhys <51117937+AguiarPhys@users.noreply.github.com>
2026-03-05 13:27:28 +0000

    Deploying to gh-pages from @ AguiarPhys/AguiarPhys.github.io@f4497d7753a32dc7fdc1c727f267df17f542cc78 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "",
